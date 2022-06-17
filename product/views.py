1bece23699373c227c704431aa4349a2ea816bb7
mehran kamrani <m.kamrani1421@gmail.com>
2022-06-17 15:43:34 +0430

    Write view for show list nested category

--- product/views.py
+++ product/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ViewSet
+from rest_framework.permissions import IsAdminUser, IsAuthenticated
+from django.db.models import Q
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from .models import Category
+from .serializers import (
+    CategoryListSerializer
+)
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+
+class CategoryViewSet(ViewSet):
+    lookup_field = 'slug'
+
+    def list(self, request):
+        category_list = Category.objects.active()
+        serializer = CategoryListSerializer(instance=category_list, context={'request': request}, many=True)
+        return Response(serializer.data)
