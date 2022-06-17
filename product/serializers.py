baab611d307fb89b3e1c1e99780e6ed34465eed0
mehran kamrani <m.kamrani1421@gmail.com>
2022-06-17 18:58:40 +0430

    Write create and update category object

--- product/serializers.py
+++ product/serializers.py
@@ -28,3 +28,32 @@ class CategoryListSerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = ('name', 'slug', 'parent')
+
+
+class CategoryInputSerializers(serializers.Serializer):
+    name = serializers.CharField(max_length=256)
+    slug = serializers.SlugField(max_length=256)
+    parent_name = serializers.CharField(max_length=256)
+
+    def create(self, validated_data):
+        name = validated_data.get('name')
+        slug = validated_data.get('slug')
+        parent_name = validated_data.get('parent_name', None)
+
+        parent = get_object_or_404(Category, is_active=True, name=parent_name)
+        category = Category.objects.create(name=name, slug=slug, parent=parent)
+
+        return category
+
+    def update(self, instance, validated_data):
+        name = validated_data.get('name')
+        slug = validated_data.get('slug')
+        parent_name = validated_data.get('parent_name', None)
+
+        category = get_object_or_404(Category, slug=slug, name=parent_name)
+        instance.name = name
+        instance.slug = slug
+        instance.parent = category
+
+        instance.save()
+        return instance
--- product/views.py
+++ product/views.py
@@ -1,11 +1,13 @@
 from rest_framework.viewsets import ViewSet
+from rest_framework import status
 from rest_framework.permissions import IsAdminUser, IsAuthenticated
 from django.db.models import Q
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from .models import Category
 from .serializers import (
-    CategoryListSerializer
+    CategoryListSerializer,
+    CategoryInputSerializers
 )
 from django.shortcuts import get_object_or_404
 
@@ -16,4 +18,27 @@ class CategoryViewSet(ViewSet):
     def list(self, request):
         category_list = Category.objects.active()
         serializer = CategoryListSerializer(instance=category_list, context={'request': request}, many=True)
-        return Response(serializer.data)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def create(self, request):
+        """
+        create category object
+        :param name:char, slug:char, parent_name:char
+        if there is not parent_name ==> error 404
+        parent_name can be None
+        """
+        serializer = CategoryInputSerializers(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'status': 'create category object successfully'}, status=status.HTTP_200_OK)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def update(self, request, slug=None):
+        category = get_object_or_404(Category, slug=slug)
+        serializer = CategoryInputSerializers(category, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
