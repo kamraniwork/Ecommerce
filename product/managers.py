dbba9ca5bc00b8d679d1be066bfccb7dc5a9df28
mehran kamrani <m.kamrani1421@gmail.com>
2022-06-18 10:32:42 +0430

    Write manager for product_type model

--- product/managers.py
+++ product/managers.py
@@ -4,3 +4,8 @@ from django.db import models
 class CategoryManager(models.Manager):
     def active(self):
         return self.filter(parent__isnull=True, is_active=True)
+
+
+class ProductTypeManager(models.Manager):
+    def active(self):
+        return self.filter(is_active=True)
